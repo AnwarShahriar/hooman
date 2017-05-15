345489d0a7459441de9025146e8bfb26226a2866
anwarshahriar <anwarshahriar@gmail.com>
2017-05-15 22:37:56 +0600

    Added post method

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var express = require('express')
+var crypto = require('crypto')
+var bodyParser = require('body-parser')
 var app = express()
+
+app.use(bodyParser.json())
 
 var humans = []
 
 app.get('/humans', function (req, res) {
     res.json(humans)
+})
+
+app.post('/humans', function (req, res) {
+    var human = req.body
+    if (!human.name) {
+        return res.status(400).send({
+            error: "Name of the human is not provided"
+        })
+    }
+    var id = crypto.randomBytes(20).toString('hex')
+    var human = {
+        id: id,
+        name: human.name
+    }
+    humans.push(human)
+    res.json(human)
 })
 
 var port = process.env.PORT || 3000
